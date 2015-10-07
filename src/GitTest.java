3af2322ecc701f1f9f4a47aa4bdbdbc7787bbb05
Amy <duyao_dy@163.com>
2015-10-07 18:27:30 +0800

    why
    can't add
    
    
    
    
    Signed-off-by: Amy <duyao_dy@163.com>

---
+++ src/GitTest.java
@@ -0,0 +1,11 @@
+
+public class GitTest {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("git test");
+		//prepare to add sth
+				System.out.println("add sth");
+	}
+
+}
